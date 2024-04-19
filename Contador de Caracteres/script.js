c10113f91eb64c19ba48f93cd4d3c1f6c2cc54fd
Dudxxx <130068099+Dudxxx@users.noreply.github.com>
2024-04-19 09:13:54 -0300

    a

--- Contador de Caracteres/script.js
+++ Contador de Caracteres/script.js
@@ -1,6 +1,6 @@
 const inputTexto = document.querySelector("#input")
-const contador = documento.querySelector("[txt-contador]")
-const btnContar = documento.querySelector("[btn-contar]")
+const contador = document.querySelector("[txt-contador]")
+const btnContar = document.querySelector("[btn-contar]")
 
 inputTexto.addEventListener("input", () => {
     console.log(String(inputTexto.value).length)
