4881626668bb774029d49d048ecacd757d54fefd
Dudxxx <130068099+Dudxxx@users.noreply.github.com>
2024-04-19 09:44:41 -0300

    contar palavras finalizado

--- Contador de Caracteres/script.js
+++
@@ -1,7 +0,0 @@
-const inputTexto = document.querySelector("#input")
-const contador = document.querySelector("[txt-contador]")
-const btnContar = document.querySelector("[btn-contar]")
-
-inputTexto.addEventListener("input", () => {
-    console.log(String(inputTexto.value).length)
-})
---
+++ Contador de Caracteres/scripts.js
@@ -0,0 +1,34 @@
+const inputTexto = document.querySelector("#input")
+const divContador = document.querySelector("[txt-contador]")
+const btnContar = document.querySelector("[btn-contar")
+
+inputTexto.addEventListener("input", palavras)
+
+btnContar.addEventListener("click", mudaBtn)
+
+function palavras() {
+    switch (btnContar.innerText) {
+        case "Contar Caracteres":
+            divContador.innerText = String(inputTexto.value).length + " caracteres"
+            break;
+        default:
+            divContador.innerText = String(inputTexto.value).trim().split(/\s+/).length + " Palavras"
+            break;
+    }
+}
+
+function mudaBtn() {
+    switch (btnContar.innerText) {
+        case "Contar Caracteres":
+            btnContar.innerText = "Contar Palavras"
+            palavras()
+            break;
+        default:
+            btnContar.innerText = "Contar Caracteres"
+            palavras()
+    }
+}
+
+
+
+
