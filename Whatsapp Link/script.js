4914498ba8c79efa8bf5a07d925a01cf6959d7f3
Dudxxx <130068099+Dudxxx@users.noreply.github.com>
2024-05-01 17:03:45 -0300

    ctt

--- Whatsapp Link/script.js
+++ Whatsapp Link/script.js
@@ -6,6 +6,7 @@ const gerarLink = document.querySelector("#gerapagina");
 const mostrarLink = document.querySelector("#geralink")
 var container = document.querySelector("#container_exibir");
 var texto = document.querySelector("#linktela")
+var copiarlink = document.querySelector("#copiarLink")
 
 telefone.addEventListener("input", ()=>{
   // Limpa todos os caracteres que não são dígitos
@@ -13,23 +14,25 @@ telefone.addEventListener("input", ()=>{
 
   if (numeroTelefone.length > 0) {
     let telefoneFormatado = `(${numeroTelefone.substring(0, 2)})${numeroTelefone.substring(2, 7)}-${numeroTelefone.substring(7, 11)}`;
-  
     telefone.value = telefoneFormatado;
   }
+  
 });
 
 gerarLink.addEventListener("click", () => {
     //tira os caracteres: "()-"
-    telefone_att = telefone.value.replace(/[^0-9]/g, '');
-    //complementa o link
-    const url_base = "https://wa.me/";
-    const url_final = url_base + "55" + telefone_att;
+    if(telefone.value.length === 14) {
+      telefone_att = telefone.value.replace(/[^0-9]/g, '');
+      //complementa o link
+      const url_base = "https://wa.me/";
+      const url_final = url_base + "55" + telefone_att;
 
 
-    //copia o link na barra de tranferencia
-    navigator.clipboard.writeText(url_final).then
-    //abre o link
-    abreLink(url_final)
+      //copia o link na barra de tranferencia
+      navigator.clipboard.writeText(url_final).then
+      //abre o link
+      abreLink(url_final)
+    }
   });
 
 
@@ -42,18 +45,32 @@ function abreLink(link) {
 
 mostrarLink.addEventListener("click", () => {
     //tira os caracteres: "()-"
-    telefone_att = telefone.value.replace(/[^0-9]/g, '');
-    //complementa o link
-    const url_base = "https://wa.me/";
-    const url_final = url_base + "55" + telefone_att;
+    if(telefone.value.length === 14) {
+        telefone_att = telefone.value.replace(/[^0-9]/g, '');
+      //complementa o link
+      const url_base = "https://wa.me/";
+      const url_final = url_base + "55" + telefone_att;
 
-    texto = strin
+      texto.textContent = url_final;
+      
 
 
-
-    if(container.style.display === "none"){
-      container.style.display = "block";
+      if(container.style.display === "none"){
+        container.style.display = "block";
+      }else {
+        container.style.display = "none";
+      }
     }else {
-      container.style.display = "none";
+
     }
+    
 }) 
+
+copiarlink.addEventListener("click", () => {
+  console.log(texto)
+  const textocopia = texto.textContent
+  navigator.clipboard.writeText(textocopia).then
+  copiarlink.textContent = "Link copiado para área de transferência!"
+  copiarlink.style.color = "black"
+
+})
